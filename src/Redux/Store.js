0ea11c22d5d878feeeaabaa45d9c70a72e05cc7e
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-07-15 13:02:49 +0530

    redux store for errorreducer

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,11 +2,13 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
 import { customerReducer } from "./Customer/Reducer";
+import { errorReducer } from "./Error/Reducer";
 
 
 const rootReducer=combineReducers({
    auth:authReducer,
-   customer:customerReducer
+   customer:customerReducer,
+   error:errorReducer
 });
 
 export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
